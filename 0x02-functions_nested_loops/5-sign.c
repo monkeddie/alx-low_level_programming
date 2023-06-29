63b14c93df6cce05c6a07a848a056299d9a827f3
edward <akazuee@gmail.com>
2023-06-29 03:21:18 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,17 +10,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar('+');
+		_putchar('+');
 		return 1; /* n is greater than zero */
 	}
 	else if (n == 0)
 	{
-		putchar('0');
+		_putchar('0');
 		return 0; /* n is zero */
 	}
 	else
 	{
-		putchar('-');
+		_putchar('-');
 		return -1; /* n is less than zero */
 	}
 }
