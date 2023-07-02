3a0ef0bd45d4027e9635a9c235599b3b8d578e48
edward <akazuee@gmail.com>
2023-07-02 11:21:24 -0700

    Write a program that prints the first 50 Fibonacci numbers, starting with 1 and 2, followed by a new line.

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: code to prinf 50 fibonacci numbers
+ * Return: Always 0
+ **/
+void fibonacci_sequence(int n, int sequence[]);
+int main(void)
+{
+	int fib_sequence[50];
+
+	fib_sequence[0] = 1;  /** First number */
+	fib_sequence[1] = 2;  /** Second number */
+
+	/** Generate the Fibonacci sequence */
+	fibonacci_sequence(50, fib_sequence);
+
+	/** Print the Fibonacci numbers separated by comma and space */
+	for (int i = 2; i < 50; i++)
+	{
+	printf("%d", fib_sequence[i]);
+	if (i != 49)
+		{
+		printf(", ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
