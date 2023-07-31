feef36d5a4c99f9e169d6c0f1735daf5dbab1c8f
edward <akazuee@gmail.com>
2023-07-31 06:49:08 -0700

    0x10. C - Variadic functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -11,10 +11,7 @@ void print_all(const char * const format, ...)
 {
 	int i = 0;
 	char *str, *sep = "";
-
-
 	va_list list;
-
 
 	va_start(list, format);
 
@@ -48,8 +45,6 @@ void print_all(const char * const format, ...)
 			i++;
 		}
 	}
-
-
 	printf("\n");
 	va_end(list);
 }
